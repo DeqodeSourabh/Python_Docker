92f8e2f74f761adc2f57fbd341b340c41a1a531e
SourabhDeqode <sogupta@deqode.com>
2022-02-14 11:38:25 +0530

    local cluster_file

--- Docker_Container_1/cluster_setup.py
+++ Docker_Container_1/cluster_setup.py
@@ -2,17 +2,22 @@ import ray
 import os 
 import time
 
+port = 6366
+workers_ips = []  #This we can use when we have workers ip Or deploy On cloud
+head_ip= "192.168.240.165"
+max_workers = 5
+min_workers = 0
 
 def start_node():
-    os.system("gnome-terminal -e 'bash -c \"ray start --head; exec bash\"'")
+    stop_node()
+    os.system("gnome-terminal -e 'bash -c \"ray start --head --port %d ; exec bash\"'"%(port))
     time.sleep(2)
 
-
 def create_multiple_nodes():
-    workers=2
-    for i in range(workers):
-        os.system("gnome-terminal -e 'bash -c \"ray start --address='192.168.240.165:6379' --redis-password='5241590000000000'; exec bash\"'")
+    for i in range(max_workers):
+        os.system("gnome-terminal -e 'bash -c \"ray start --address='%s:%d' --redis-password='5241590000000000'; exec bash\"'" %(head_ip,port))
     time.sleep(3)
 
 def stop_node():
     os.system("gnome-terminal -e 'bash -c \"ray stop; exec bash\"'")
+
